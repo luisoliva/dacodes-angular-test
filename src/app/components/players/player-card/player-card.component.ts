ede4afd0170bb9d0f478b92d44b1f82da464d6b6
luisoliva <luisalbertolivamedina@hotmail.com>
2020-04-09 23:21:56 -0500

    Se termina la UI del modulo de jugadores

--- src/app/components/players/player-card/player-card.component.ts
+++ src/app/components/players/player-card/player-card.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {PlayerModel} from '../../../core/models/Player.model';
+import {CoachModel} from '../../../core/models/Coach.model';
 
 @Component({
   selector: 'app-player-card',
@@ -7,11 +8,21 @@ import {PlayerModel} from '../../../core/models/Player.model';
   styleUrls: ['./player-card.component.css']
 })
 export class PlayerCardComponent implements OnInit {
-    @Input() player: PlayerModel;
-
+    @Input() model: any;
+    player: Partial<PlayerModel>;
+    coach: Partial<CoachModel>;
+    isCoach: boolean;
     constructor() { }
 
     ngOnInit(): void {
+        debugger
+        if(this.model.hasOwnProperty('role')){
+            this.isCoach = true;
+            this.coach = this.model;
+        }else{
+            this.player = this.model;
+            this.isCoach = false;
+        }
     }
 
 }
